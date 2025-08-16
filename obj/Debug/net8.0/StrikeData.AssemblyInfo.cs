b250a90a2a5415c78496474357176ff2e9d36b51
braiss13 <bsampayolorenzo@gmail.com>
2025-08-16 14:49:02 +0200

    Modifying Others

--- obj/Debug/net8.0/StrikeData.AssemblyInfo.cs
+++ obj/Debug/net8.0/StrikeData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StrikeData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f35ce2fcfc58d262e415d1c7f2cb560709d7e3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af04ed2d6b7d07a6322846b5a92f812d835cbfdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StrikeData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StrikeData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
